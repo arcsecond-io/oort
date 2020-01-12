71ccd352b8272c40213c63acdd6c117f2fe9f22f
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-01-12 10:20:01 +0100

    Custom mustaches

--- oort/app/__init__.py
+++ oort/app/__init__.py
@@ -2,7 +2,16 @@ from flask import Flask
 from pony.flask import Pony
 from .config import config
 
-app = Flask(__name__)
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        variable_start_string='{$',  # Default is '{{', I'm changing this because Vue.js uses '{{' / '}}'
+        variable_end_string='$}',
+    ))
+
+
+app = CustomFlask(__name__)
 app.config.update(config)
 
 Pony(app)
