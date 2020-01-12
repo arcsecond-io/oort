bf7d0409ad5844ca77be83edcfb613a86609ff71
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-01-12 19:37:23 +0100

    Providing folder

--- oort/server.py
+++ oort/server.py
@@ -20,15 +20,20 @@ pass_state = click.make_pass_decorator(State, ensure=True)
 VERSION_HELP_STRING = "Show the Oort Cloud version and exit."
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+
 @click.group(invoke_without_command=True, context_settings=CONTEXT_SETTINGS)
 @click.option('-v', '--version', is_flag=True, help=VERSION_HELP_STRING)
 @basic_options
+@pass_state
 @click.pass_context
-def main(ctx, version=False, v=False):
-    print(os.getcwd())
+def main(ctx, state, version=False, v=False):
     if version or v:
         click.echo(__version__)
     elif ctx.invoked_subcommand is None:
         db.bind(**app.config['PONY'])
         db.generate_mapping(create_tables=True)
-        app.run()
+        port = 5000
+        while is_port_in_use(port):
+            port += 1
+        app.config['folder'] = os.getcwd()
+        app.run(debug=state.debug, port=port)
