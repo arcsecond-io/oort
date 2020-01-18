f46cc408bc6a1ce390269d6ead50f13260ddfd60
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-01-18 18:32:31 +0100

    Managing to change progress bar width!

--- oort/app/static/js/app.js
+++ oort/app/static/js/app.js
@@ -11,6 +11,13 @@ var app = new Vue({
     this.source_active = new EventSource('/uploads/active')
     this.source_active.onmessage = function (event) {
       self.uploads_active = JSON.parse(event.data)
+      const bars = document.getElementsByClassName('progress-bar')
+      self.uploads_active.forEach((upload, index) => {
+        let bar = bars[index]
+        if (bar) {
+          bar.style.width = upload.progress.toFixed(1).toString() + '%'
+        }
+      })
     }
     this.source_inactive = new EventSource('/uploads/inactive')
     this.source_inactive.onmessage = function (event) {
