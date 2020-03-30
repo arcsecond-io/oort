f1bb56f9b031b56e2a04c3aacd3aa10101c64802
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-03-30 18:57:18 +0200

    No need for separate object
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/static/js/admin.js
+++ oort/app/static/js/admin.js
@@ -4,14 +4,13 @@ var admin = new Vue({
     admin: null,
     source: null
   },
-  mounted: function () {
+  created: function () {
     const self = this
     this.source = new EventSource('/admin')
     this.source.onmessage = function (event) {
       const json = JSON.parse(event.data)
       console.log(json)
       self.admin = json.admin
-      self.context = json.context
     }
   }
 })
