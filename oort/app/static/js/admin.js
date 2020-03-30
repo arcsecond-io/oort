3ac5f1f0fb034ca2172c180ef00eab08ca58978c
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-03-30 22:20:18 +0200

    Allowing to close event source
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/static/js/admin.js
+++ oort/app/static/js/admin.js
@@ -4,13 +4,18 @@ var admin = new Vue({
     admin: null,
     source: null
   },
-  created: function () {
+  mounted: function () {
     const self = this
     this.source = new EventSource('/admin')
     this.source.onmessage = function (event) {
       const json = JSON.parse(event.data)
       console.log(json)
       self.admin = json.admin
+      if (self.admin.close) {
+        self.source.close()
+        self.source.onmessage = null
+        self.source = null
+      }
     }
   }
 })
