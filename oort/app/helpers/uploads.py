e1c864e464cd9aa44a569e93741d827fd2028d48
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-03-31 19:30:21 +0200

    Refactor of uploads state
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/helpers/uploads.py
+++ oort/app/helpers/uploads.py
@@ -1,9 +1,84 @@
+import os
 from arcsecond import Arcsecond
 
 from .state import LocalState
+
+#
+# def wrap_files(debug, folder, dataset_uuid, autostart=True):
+#     files = os.listdir(folder)
+#     for file in files:
+#         filepath = os.path.join(folder, file)
+#         fw = UPLOADS.get(filepath)
+#         if fw is None:
+#             fw = FileWrapper(filepath, dataset_uuid, debug)
+#             UPLOADS[filepath] = fw
+#
+#         started_count = len([u for u in UPLOADS.values() if u.is_started()])
+#         if autostart and started_count < MAX_SIMULTANEOUS_UPLOADS:
+#             fw.start()
+#         if fw.will_finish():
+#             fw.finish()
+
+
+ROOT_FILES_SECTION = '__root__'
 
 
 class UploadsLocalState(LocalState):
     def __init__(self, config):
         super().__init__(config)
         self.update_payload('state', self.context.to_dict())
+        self.update_payload('showTables', False, 'state')
+        self.update_payload('current_uploads', [])
+        self.update_payload('finished_uploads', [])
+
+        self.files = {}
+        self.api_datasets = Arcsecond.build_datasets_api(debug=self.context.debug,
+                                                         organisation=self.context.organisation)
+
+    def _read_local_files(self, section):
+        suffix = section if section != ROOT_FILES_SECTION else ''
+        names = os.listdir(os.path.join(self.context.folder, suffix))
+        for name in names:
+            path = os.path.join(self.context.folder, name)
+            if os.path.isdir(path):
+                if section == ROOT_FILES_SECTION:
+                    self._read_local_files(name)
+            else:
+                if section not in self.files.keys():
+                    self.files[section] = []
+                if len(name) > 0 and name[0] != '.' and path not in self.files[section] and os.path.isfile(path):
+                    self.files[section].append(path)
+
+    def sync_datasets(self):
+        if not self.context.canUpload:
+            return
+
+        local_nightlog = self.read('night_log')
+        if not local_nightlog:
+            return
+
+        self._read_local_files(ROOT_FILES_SECTION)
+        print(self.files)
+
+        # api_datasets = Arcsecond.build_datasets_api(debug=debug)
+        # all_datasets = api_datasets.list()
+        # upload_dataset = next((d for d in all_datasets if d['name'] == DATASET_NAME), None)
+        # if not upload_dataset:
+        #     msg = f'Dataset "{DATASET_NAME}" does not exist. Creating it...'
+        #     state = {'message': msg, 'showTables': False}
+        #     json_data = json.dumps({'state': state, 'uploads': [], 'finished_uploads': []})
+        #     yield f"data:{json_data}\n\n"
+        #     upload_dataset = api_datasets.create({'name': DATASET_NAME})
+        # else:
+        #     msg = f'Dataset "{DATASET_NAME}" exists. Walking through local files...'
+        #     state = {'message': msg, 'showTables': False}
+        #     json_data = json.dumps({'state': state, 'uploads': [], 'finished_uploads': []})
+        #     yield f"data:{json_data}\n\n"
+        #
+        # while True:
+        #     wrap_files(debug, folder, upload_dataset['uuid'])
+        #     uploads_data = [fw.to_dict() for fw in UPLOADS.values() if fw.is_finished() is False]
+        #     finished_uploads_data = [fw.to_dict() for fw in UPLOADS.values() if fw.is_finished() is True]
+        #     state = {'message': '', 'showTables': True}
+        #     json_data = json.dumps(
+        #         {'state': state, 'uploads': uploads_data, 'finished_uploads': finished_uploads_data})
--- oort/app/views.py
+++ oort/app/views.py
@@ -7,7 +7,7 @@ from flask import current_app as app
 
 from arcsecond import Arcsecond
 
-from oort.app.helpers import AdminLocalState, Context, FileWrapper
+from oort.app.helpers import AdminLocalState, UploadsLocalState, Context, FileWrapper
 
 main = Blueprint('main', __name__)
 
@@ -38,67 +38,15 @@ def admin():
     return Response(generate(), mimetype='text/event-stream')
 
 
-def wrap_files(debug, folder, dataset_uuid, autostart=True):
-    files = os.listdir(folder)
-    for file in files:
-        filepath = os.path.join(folder, file)
-        fw = UPLOADS.get(filepath)
-        if fw is None:
-            fw = FileWrapper(filepath, dataset_uuid, debug)
-            UPLOADS[filepath] = fw
-
-        started_count = len([u for u in UPLOADS.values() if u.is_started()])
-        if autostart and started_count < MAX_SIMULTANEOUS_UPLOADS:
-            fw.start()
-        if fw.will_finish():
-            fw.finish()
-
-
 @main.route('/uploads')
 def uploads():
-    config = app.config
-    debug = config['debug']
-    folder = config['folder']
-    canUpload = Context(config).canUpload
+    state = UploadsLocalState(app.config)
 
     def generate():
-        global UPLOADS
-
-        if canUpload is False:
-            state = {'message': '', 'showTables': False}
-            json_data = json.dumps({'state': state, 'uploads': [], 'finished_uploads': []})
-            yield f"data:{json_data}\n\n"
-
-        else:
-            msg = 'Initializing (listing existing Datasets)...'
-            state = {'message': msg, 'showTables': False}
-            json_data = json.dumps({'state': state, 'uploads': [], 'finished_uploads': []})
-            yield f"data:{json_data}\n\n"
-
-            api_datasets = Arcsecond.build_datasets_api(debug=debug)
-            all_datasets = api_datasets.list()
-            upload_dataset = next((d for d in all_datasets if d['name'] == DATASET_NAME), None)
-            if not upload_dataset:
-                msg = f'Dataset "{DATASET_NAME}" does not exist. Creating it...'
-                state = {'message': msg, 'showTables': False}
-                json_data = json.dumps({'state': state, 'uploads': [], 'finished_uploads': []})
-                yield f"data:{json_data}\n\n"
-                upload_dataset = api_datasets.create({'name': DATASET_NAME})
-            else:
-                msg = f'Dataset "{DATASET_NAME}" exists. Walking through local files...'
-                state = {'message': msg, 'showTables': False}
-                json_data = json.dumps({'state': state, 'uploads': [], 'finished_uploads': []})
-                yield f"data:{json_data}\n\n"
-
-            while True:
-                wrap_files(debug, folder, upload_dataset['uuid'])
-                uploads_data = [fw.to_dict() for fw in UPLOADS.values() if fw.is_finished() is False]
-                finished_uploads_data = [fw.to_dict() for fw in UPLOADS.values() if fw.is_finished() is True]
-                state = {'message': '', 'showTables': True}
-                json_data = json.dumps(
-                    {'state': state, 'uploads': uploads_data, 'finished_uploads': finished_uploads_data})
-                yield f"data:{json_data}\n\n"
-                time.sleep(1)
+        while True:
+            state.sync_datasets()
+            yield state.get_yield_string()
+            time.sleep(1)
 
     # Using Server-Side Events. See https://blog.easyaspy.org/post/10/2019-04-30-creating-real-time-charts-with-flask
     return Response(generate(), mimetype='text/event-stream')
