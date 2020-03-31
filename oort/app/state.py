bc402e2a21713cfd31655c5282d5672e2a72b7c4
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-03-31 19:01:34 +0200

    Refactor for an easier check of telescope and night log
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/state.py
+++ oort/app/state.py
@@ -1,14 +1,48 @@
+import json
 import os
+import datetime
 from configparser import ConfigParser
 
+from arcsecond import Arcsecond
 
-class State:
-    def __init__(self, organisation, debug):
-        self.organisation = organisation
-        self.debug = debug
-        raw_section = 'debug' if debug else 'main'
-        self._section = 'organisation:' + organisation + ':' + raw_section if organisation else raw_section
+
+class Context:
+    def __init__(self, config):
+        self.debug = config['debug']
+        self.folder = config['folder']
+        self.organisation = config['organisation']
+        self.telescope = config['telescope']
+
+        self.username = Arcsecond.username(debug=self.debug)
+        self.isAuthenticated = Arcsecond.is_logged_in(debug=self.debug)
+        self.memberships = Arcsecond.memberships(debug=self.debug)
+
+        self.role = None
+        if self.organisation is not None:
+            self.role = self.memberships.get(self.organisation, None)
+
+        self.canUpload = self.organisation is None or self.role in ['member', 'admin', 'superadmin']
+
+    def to_dict(self):
+        return {'folder': self.folder,
+                'isAuthenticated': self.isAuthenticated,
+                'username': self.username,
+                'organisation': self.organisation,
+                'role': self.role,
+                'telescope': self.telescope,
+                'canUpload': self.canUpload}
+
+
+class LocalState:
+    def __init__(self, config):
+        self.context = Context(config)
         self._config_filepath = os.path.expanduser('.oort.ini')
+        self.payload = {}
+
+    @property
+    def _section(self):
+        raw_section = 'debug' if self.context.debug else 'main'
+        return 'organisation:' + self.context.organisation + ':' + raw_section if self.context.organisation else raw_section
 
     def _get_config(self):
         _config = ConfigParser()
@@ -32,3 +66,94 @@ class State:
         for k, v in kwargs.items():
             config.set(self._section, k, v)
         self._save_config(config)
+
+    def update_payload(self, key, value, group=None):
+        if group:
+            if group not in self.payload.keys():
+                self.payload[group] = {}
+            self.payload[group][key] = value
+        else:
+            self.payload[key] = value
+
+    def get_yield_string(self):
+        json_data = json.dumps(self.payload)
+        return f"data:{json_data}\n\n"
+
+
+class AdminLocalState(LocalState):
+    def __init__(self, config):
+        super().__init__(config)
+        self.update_payload('admin', self.context.to_dict())
+
+        self.telescopes_api = Arcsecond.build_telescopes_api(debug=self.context.debug,
+                                                             organisation=self.context.organisation)
+
+        self.logs_api = Arcsecond.build_nightlogs_api(debug=self.context.debug,
+                                                      organisation=self.context.organisation)
+
+    def _check_remote_telescope(self, ):
+        telescope, error = self.telescopes_api.read(self.context.telescope)
+        if error:
+            self.update_payload('message', str(error), 'admin')
+        elif telescope:
+            self.update_payload('message', '', 'admin')
+            self.update_payload('telescope', telescope, 'admin')
+            self.save(telescope=json.dumps(telescope))
+        else:
+            self.update_payload('message', f"Unknown telescope with UUID {self.context.telescope}", 'admin')
+
+    def sync_telescope(self):
+        self.update_payload('message', '', 'admin')
+        local_telescope = json.loads(self.read('telescope') or '{}')
+
+        if local_telescope and self.context.telescope:
+            # Check if they are the same
+            if local_telescope['uuid'] == self.context.telescope:
+                self.update_payload('telescope', local_telescope, 'admin')
+            else:
+                # If not, telescope provided in CLI takes precedence.
+                self._check_remote_telescope()
+
+        elif local_telescope and not self.context.telescope:
+            self.update_payload('telescope', local_telescope, 'admin')
+
+        elif not local_telescope and self.context.telescope:
+            self._check_remote_telescope()
+
+        else:
+            # Do nothing
+            pass
+
+    def sync_night_log(self):
+        local_telescope = json.loads(self.read('telescope') or '{}')
+        if not local_telescope:
+            return
+
+        before_noon = datetime.datetime.now().hour < 12
+        if before_noon:
+            date = (datetime.datetime.now() - datetime.timedelta(days=1)).date().isoformat()
+        else:
+            date = datetime.datetime.now().date().isoformat()
+
+        self.update_payload('date', date, 'admin')
+        local_nightlog = json.loads(self.read('night_log') or '{}')
+
+        if local_nightlog:
+            self.update_payload('night_log', local_nightlog, 'admin')
+        else:
+            logs, error = self.logs_api.list(date=date)
+
+            if error:
+                self.update_payload('message', str(error), 'admin')
+            elif len(logs) == 0:
+                result, error = self.logs_api.create({'date': date, 'telescope': local_telescope['uuid']})
+                if error:
+                    pass
+                elif result:
+                    self.save(night_log=json.dumps(result))
+                    self.update_payload('night_log', result, 'admin')
+            elif len(logs) == 1:
+                self.save(night_log=json.dumps(logs[0]))
+                self.update_payload('night_log', logs[0], 'admin')
+            else:
+                self.update_payload('message', f'Multiple logs found for date {date}', 'admin')
--- oort/app/views.py
+++ oort/app/views.py
@@ -9,40 +9,13 @@ from flask import current_app as app
 from arcsecond import Arcsecond
 
 from .models import FileWrapper
-from .state import State
+from .state import AdminLocalState, Context
 
 main = Blueprint('main', __name__)
 
 DATASET_NAME = 'Oort Uploads'
 MAX_SIMULTANEOUS_UPLOADS = 3
 UPLOADS = {}
-
-
-class Context:
-    def __init__(self, config):
-        self.debug = config['debug']
-        self.folder = app.config['folder']
-        self.organisation = config['organisation']
-        self.telescope = config['telescope']
-
-        self.username = Arcsecond.username(debug=self.debug)
-        self.isAuthenticated = Arcsecond.is_logged_in(debug=self.debug)
-        self.memberships = Arcsecond.memberships(debug=self.debug)
-
-        self.role = None
-        if self.organisation is not None:
-            self.role = self.memberships.get(self.organisation, None)
-
-        self.canUpload = self.organisation is None or self.role in ['member', 'admin', 'superadmin']
-
-    def to_dict(self):
-        return {'folder': self.folder,
-                'isAuthenticated': self.isAuthenticated,
-                'username': self.username,
-                'organisation': self.organisation,
-                'role': self.role,
-                'telescope': self.telescope,
-                'canUpload': self.canUpload}
 
 
 @main.route('/')
@@ -53,97 +26,15 @@ def index():
 
 @main.route('/admin')
 def admin():
-    context = Context(app.config)
-    state = State(context.organisation, context.debug)
+    state = AdminLocalState(app.config)
 
     def generate():
-        admin = {'message': '', 'close': False}
-        admin.update(**context.to_dict())
-
-        if context.organisation and context.role:
-            # Auto Handling of Night Logs !
-
-            # --- Telescope --
-
-            local_telescope = json.loads(state.read('telescope') or '{}')
-            telescopes_api = Arcsecond.build_telescopes_api(debug=context.debug, organisation=context.organisation)
-
-            if local_telescope:
-                if local_telescope['uuid'] == context.telescope:
-                    admin['telescope'] = local_telescope
-                else:
-                    telescope = telescopes_api.read(context.telescope)
-                    if telescope:
-                        admin['telescope'] = local_telescope
-                        state.save(telescope=json.dumps(local_telescope))
-                        # TODO: check telescope belongs to organisation
-                    else:
-                        admin['message'] = f"Bummer..."
-                        admin['close'] = True
-
-            else:
-                if context.telescope:
-                    telescope = telescopes_api.read(context.telescope)
-                    if telescope:
-                        admin['telescope'] = telescope
-                        state.save(telescope=json.dumps(telescope))
-                        # TODO: check telescope belongs to organisation
-                    else:
-                        admin['message'] = f"Bummer..."
-                        admin['close'] = True
-                else:
-                    admin['message'] = 'Fetching Organisation\'s Telescopes...'
-                    json_data = json.dumps({'admin': admin})
-                    yield f"data:{json_data}\n\n"
-
-                    telescopes = telescopes_api.list()
-                    if len(telescopes) == 0:
-                        admin['message'] = "No telescopes found found."
-                    elif len(telescopes) == 1:
-                        admin['telescope'] = telescopes[0]
-                        state.save(telescope=json.dumps(telescopes[0]))
-                    else:
-                        admin['message'] = f"Multiple telescopes found. {telescopes}"
-                        admin['close'] = True
-
-                    json_data = json.dumps({'admin': admin})
-                    yield f"data:{json_data}\n\n"
-
-            date = datetime.datetime.now().date().isoformat()
-            admin['date'] = date
-            json_data = json.dumps({'admin': admin})
-            yield f"data:{json_data}\n\n"
-
-            # --- Night Log --
-
-            log = json.loads(state.read('night_log') or '{}')
-            if log:
-                admin['night_log'] = log
-            else:
-                logs_api = Arcsecond.build_nightlogs_api(debug=context.debug, organisation=context.organisation)
-                logs = logs_api.list(date=date)
-
-                if len(logs) == 0:
-                    admin['message'] = f'Found no Night Log for date {date}. Creating one...'
-                elif len(logs) == 1:
-                    state.save(night_log=json.dumps(logs[0]))
-                    admin['night_log'] = logs[0]
-                else:
-                    admin['message'] = f'Multiple logs found for date {date}'
-
-                json_data = json.dumps({'admin': admin})
-                yield f"data:{json_data}\n\n"
-
-                if len(logs) == 0 and admin['telescope']:
-                    print(admin['telescope'])
-                    log = logs_api.create({'date': date, 'telescope': admin['telescope']['uuid']})
-                    state.save(night_log=json.dumps(log))
-
-            # --- Datasets --
-
-        admin['close'] = True
-        json_data = json.dumps({'admin': admin})
-        yield f"data:{json_data}\n\n"
+        while True:
+            state.sync_telescope()
+            yield state.get_yield_string()
+            state.sync_night_log()
+            yield state.get_yield_string()
+            time.sleep(60)
 
     # Using Server-Side Events. See https://blog.easyaspy.org/post/10/2019-04-30-creating-real-time-charts-with-flask
     return Response(generate(), mimetype='text/event-stream')
