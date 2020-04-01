da49ce3d3e4a1b7682ff0dd2e49820f913d88caf
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-04-01 23:19:50 +0200

    Usin new telescopeUUID var
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/helpers/admin.py
+++ oort/app/helpers/admin.py
@@ -18,37 +18,49 @@ class AdminLocalState(LocalState):
                                                       organisation=self.context.organisation)
 
     def _check_remote_telescope(self, ):
-        telescope, error = self.telescopes_api.read(self.context.telescope)
+        telescope, error = self.telescopes_api.read(self.context.telescopeUUID)
         if error:
+            if self.context.debug: print(str(error))
             self.update_payload('message', str(error), 'admin')
         elif telescope:
             self.update_payload('message', '', 'admin')
             self.update_payload('telescope', telescope, 'admin')
             self.save(telescope=json.dumps(telescope))
         else:
-            self.update_payload('message', f"Unknown telescope with UUID {self.context.telescope}", 'admin')
+            self.update_payload('message', f"Unknown telescope with UUID {self.context.telescopeUUID}", 'admin')
+            self.save(telescope='')
 
     def sync_telescope(self):
         self.update_payload('message', '', 'admin')
         local_telescope = json.loads(self.read('telescope') or '{}')
 
-        if local_telescope and self.context.telescope:
+        if local_telescope and self.context.telescopeUUID:
             # Check if they are the same
-            if local_telescope['uuid'] == self.context.telescope:
+            if local_telescope['uuid'] == self.context.telescopeUUID:
                 self.update_payload('telescope', local_telescope, 'admin')
             else:
-                # If not, telescope provided in CLI takes precedence.
+                # If not, telescope provided in CLI takes precedence, even if it fails...
                 self._check_remote_telescope()
 
-        elif local_telescope and not self.context.telescope:
+        elif local_telescope and not self.context.telescopeUUID:
             self.update_payload('telescope', local_telescope, 'admin')
 
-        elif not local_telescope and self.context.telescope:
+        elif not local_telescope and self.context.telescopeUUID:
             self._check_remote_telescope()
 
         else:
             # Do nothing
             pass
+
+    def _create_night_log(self, date, local_telescope):
+        result, error = self.logs_api.create({'date': date, 'telescope': local_telescope['uuid']})
+        if error:
+            if self.context.debug: print(str(error))
+            msg = f'Failed to create night log for date {date}. Retry is automatic.'
+            self.update_payload('message', msg, 'admin')
+        else:
+            self.save(night_log=json.dumps(result))
+            self.update_payload('night_log', result, 'admin')
 
     def sync_night_log(self):
         local_telescope = json.loads(self.read('telescope') or '{}')
@@ -63,34 +75,25 @@ class AdminLocalState(LocalState):
 
         self.update_payload('date', date, 'admin')
         local_nightlog = json.loads(self.read('night_log') or '{}')
+        self.save(datasets='')
 
         if local_nightlog:
             if local_nightlog['date'] == date:
                 self.update_payload('night_log', local_nightlog, 'admin')
             else:
-                self.save(datasets='')
-                result, error = self.logs_api.create({'date': date, 'telescope': local_telescope['uuid']})
-                if error:
-                    pass
-                elif result:
-                    self.save(night_log=json.dumps(result))
-                    self.update_payload('night_log', result, 'admin')
+                self._create_night_log(date, local_telescope)
         else:
             logs, error = self.logs_api.list(date=date)
 
             if error:
+                if self.context.debug: print(str(error))
                 self.update_payload('message', str(error), 'admin')
             elif len(logs) == 0:
-                self.save(datasets='')
-                result, error = self.logs_api.create({'date': date, 'telescope': local_telescope['uuid']})
-                if error:
-                    pass
-                elif result:
-                    self.save(night_log=json.dumps(result))
-                    self.update_payload('night_log', result, 'admin')
+                self._create_night_log(date, local_telescope)
             elif len(logs) == 1:
-                self.save(datasets='')
                 self.save(night_log=json.dumps(logs[0]))
                 self.update_payload('night_log', logs[0], 'admin')
             else:
-                self.update_payload('message', f'Multiple logs found for date {date}', 'admin')
+                msg = f'Multiple logs found for date {date}'
+                if self.context.debug: print(msg)
+                self.update_payload('message', msg, 'admin')
