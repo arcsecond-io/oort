f406a25cf9697c68478559441819e61e7a551af8
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-04-04 13:07:57 +0200

    Using paginated response for datasets + better handling of connection error
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/helpers/uploads.py
+++ oort/app/helpers/uploads.py
@@ -1,6 +1,8 @@
 import json
 import os
-from arcsecond import Arcsecond
+
+import arcsecond
+from arcsecond import Arcsecond, ArcsecondConnectionError
 
 from .filewrapper import FileWrapper
 from .state import LocalState
@@ -57,24 +59,30 @@ class UploadsLocalState(LocalState):
                 new_local_datasets[dataset_uuid] = dataset_name
             else:
                 night_log_uuid = local_nightlog.get('uuid')
-                existing_datasets, error = self.api_datasets.list(name=dataset_name, night_log=night_log_uuid)
-                if error:
-                    if self.context.debug: print(str(error))
-                elif len(existing_datasets) == 0:
-                    payload = {'name': dataset_name, 'night_log': local_nightlog.get('uuid')}
-                    result, error = self.api_datasets.create(payload)
+                try:
+                    existing_datasets_response, error = self.api_datasets.list(name=dataset_name, night_log=night_log_uuid)
+                except arcsecond.api.error.ArcsecondConnectionError as e:
+                    if self.context.debug: print(str(e))
+                    self.update_payload('warning', str(e), 'state')
+                else:
+                    existing_datasets = existing_datasets_response['results']
                     if error:
                         if self.context.debug: print(str(error))
-                    elif result:
-                        new_local_datasets[result['uuid']] = dataset_name
-                elif len(existing_datasets) == 1:
-                    new_local_datasets[existing_datasets[0]['uuid']] = dataset_name
-                else:
-                    msg = f'Multiple datasets found for name {dataset_name}. Choosing first created one.'
-                    if self.context.debug: print(str(error))
-                    self.update_payload('message', msg)
-                    existing_datasets.sort(key=lambda obj: obj['creation_date'])
-                    new_local_datasets[existing_datasets[0]['uuid']] = dataset_name
+                    elif len(existing_datasets) == 0:
+                        payload = {'name': dataset_name, 'night_log': local_nightlog.get('uuid')}
+                        result, error = self.api_datasets.create(payload)
+                        if error:
+                            if self.context.debug: print(str(error))
+                        elif result:
+                            new_local_datasets[result['uuid']] = dataset_name
+                    elif len(existing_datasets) == 1:
+                        new_local_datasets[existing_datasets[0]['uuid']] = dataset_name
+                    else:
+                        msg = f'Multiple datasets found for name {dataset_name}. Choosing first created one.'
+                        if self.context.debug: print(str(error))
+                        self.update_payload('warning', msg, 'state')
+                        existing_datasets.sort(key=lambda obj: obj['creation_date'])
+                        new_local_datasets[existing_datasets[0]['uuid']] = dataset_name
 
         self.save(datasets=json.dumps(new_local_datasets))
 
