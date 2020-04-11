3ad7a91f86fce7df9cb423498b727642feded4d6
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-04-11 14:55:48 +0200

    Adding an isAlive check
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/static/js/uploads.js
+++ oort/app/static/js/uploads.js
@@ -1,15 +1,22 @@
 var app = new Vue({
   el: '#uploads',
   data: {
-    state: null,
+    state: { showTables: false },
     current_uploads: [],
     finished_uploads: [],
-    source: null
+    source: null,
+    isAlive: true,
+    loopID: null
+  },
+  beforeDestroy () {
+    clearInterval(this.loopID)
   },
   mounted: function () {
     const self = this
+
     this.source = new EventSource('/uploads')
     this.source.onmessage = function (event) {
+      self.isAlive = true
       const json = JSON.parse(event.data)
       self.state = json.state
       self.current_uploads = json.current_uploads.sort((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
@@ -22,6 +29,13 @@ var app = new Vue({
         }
       })
     }
+
+    this.loopID = setInterval(function ping () {
+      self.isAlive = (self.source.readyState <= 1)
+      if (!self.isAlive) {
+        self.$forceUpdate()
+      }
+    }, 3000)
   }
 })
 
