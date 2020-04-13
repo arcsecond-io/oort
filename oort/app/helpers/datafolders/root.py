3fc994b469d8cf970d07b2a3bf5d439db7b74a43
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-04-13 12:45:03 +0200

    Making sure to walk all folders
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/helpers/datafolders/root.py
+++ oort/app/helpers/datafolders/root.py
@@ -89,9 +89,13 @@ class RootFolder(FilesWalker):
                 self.context.payload_append(night_logs=new_log)
 
     def sync_calibrations(self):
+        for telescope_folder in self.telescope_folders:
+            telescope_folder.walk()
+
         night_logs = self.context.get_payload('night_logs')
         for telescope_folder in self.telescope_folders:
             night_log = find(night_logs, telescope=telescope_folder.uuid)
+
             if night_log:
                 payload_key = f'telescope_{telescope_folder.uuid}'
                 telescope_folder.sync_calibrations(payload_key, night_log=night_log['uuid'])
