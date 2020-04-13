e17656e20e6cb960156b037e7d7fbc1201c067da
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-04-13 05:42:58 +0200

    UI tweaks + enabling telescopes and night logs again
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/static/js/uploads.js
+++ oort/app/static/js/uploads.js
@@ -5,6 +5,9 @@ var app = new Vue({
     messages: {},
     telescopes: [],
     night_logs: [],
+    selected_telescope: null,
+    selected_night_log: null,
+    selected_night_log_url: '',
     current_uploads: [],
     finished_uploads: [],
     source: null,
@@ -27,29 +30,19 @@ var app = new Vue({
       self.telescopes = json.telescopes
       self.night_logs = json.night_logs
 
-      // self.admin = json.admin
-      // if (self.admin['selected_night_log']) {
-      //   self.admin['selected_night_log_url'] = 'data/' + self.admin['selected_night_log']['date'].replace(/-/gi, '/')
-      // }
-      // if (self.admin.close) {
-      //   self.source.close()
-      //   self.source.onmessage = null
-      //   self.source = null
-      // }
-      //
-      // self.current_uploads = json.current_uploads
-      // self.finished_uploads = json.finished_uploads
-      //
-      // self.current_uploads.sort((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
-      // self.finished_uploads.sort((u1, u2) => new Date(u1.ended).getDate() < new Date(u2.ended).getDate())
-      //
-      // const bars = document.getElementsByClassName('progress-bar')
-      // self.current_uploads.forEach((upload, index) => {
-      //   let bar = bars[index]
-      //   if (bar) {
-      //     bar.style.width = upload.progress.toFixed(1).toString() + '%'
-      //   }
-      // })
+      self.current_uploads = json.current_uploads
+      self.finished_uploads = json.finished_uploads
+
+      self.current_uploads.sort((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
+      self.finished_uploads.sort((u1, u2) => new Date(u1.ended).getDate() < new Date(u2.ended).getDate())
+
+      const bars = document.getElementsByClassName('progress-bar')
+      self.current_uploads.forEach((upload, index) => {
+        let bar = bars[index]
+        if (bar) {
+          bar.style.width = upload.progress.toFixed(1).toString() + '%'
+        }
+      })
     }
 
     this.loopID = setInterval(function ping () {
@@ -61,23 +54,18 @@ var app = new Vue({
   },
   methods: {
     selectTelescope (uuid) {
-      // this.admin['selected_telescope'] = (uuid === '__all__') ? null : this.admin.telescopes.find(t => t.uuid === uuid)
-      // if (this.admin['selected_telescope']) {
-      //   this.admin['selected_night_log'] = this.admin.night_logs.find(nl => nl.telescope === uuid)
-      // } else {
-      //   this.admin['selected_night_log'] = null
-      // }
-      // if (this.admin['selected_night_log']) {
-      //   window.localStorage.setItem('selected_night_log', JSON.stringify(this.admin['selected_night_log']))
-      // } else {
-      //   window.localStorage.removeItem('selected_night_log')
-      // }
-      // if (this.admin.night_logs.length) {
-      //   this.admin['selected_night_log_url'] = 'data/' + this.admin.night_logs[0]['date'].replace(/-/gi, '/')
-      // } else {
-      //   this.admin['selected_night_log_url'] = ''
-      // }
-      // this.$forceUpdate()
+      this.selected_telescope = (uuid === '__all__') ? null : this.telescopes.find(t => t.uuid === uuid)
+      if (this.selected_telescope) {
+        this.selected_night_log = this.night_logs.find(nl => nl.telescope === uuid)
+      } else {
+        this.selected_night_log = null
+      }
+      if (this.selected_night_log) {
+        this.selected_night_log_url = 'data/' + this.night_logs[0]['date'].replace(/-/gi, '/')
+      } else {
+        this.selected_night_log_url = ''
+      }
+      this.$forceUpdate()
     }
   }
 })
