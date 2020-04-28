afd7c6235a9700173724d290e0c843037e328a4d
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-04-28 23:06:37 +0200

    Do not hit telescopes endpoint everytime after a useless reset
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/app/helpers/root.py
+++ oort/app/helpers/root.py
@@ -6,18 +6,20 @@ from arcsecond import ArcsecondConnectionError
 from .constants import OORT_FILENAME
 from .filewalker import FilesWalker
 from .telescopes import TelescopeFolder
+from .utils import find_first_in_list
 
 
 class RootFolder(FilesWalker):
     def __init__(self, context):
         super().__init__(context, None, context.folder)
+        self.reset()
 
     def reset(self):
         self.other_folders = []
         self.telescope_folders = []
 
     def find_telescope_folders(self):
-        self.reset()
+        # Do not reset every time.
         for name, path in self._walk_folder():
             # If it's a folder, check if it is a telescope one.
             if os.path.isdir(path):
@@ -60,24 +62,25 @@ class RootFolder(FilesWalker):
             return (_config['astronomer']['username'], _config['astronomer']['api_key'])
         return None
 
-    def walk_telescope_folders(self):
-        for telescope_folder in self.telescope_folders:
-            telescope_folder.walk()
-
     def read_remote_telescopes(self):
         self.context.messages['warning'] = ''
         self.context.telescopes = []
 
         try:
             for telescope_folder in self.telescope_folders:
-                telescope_folder.read_remote_telescope()
+                if find_first_in_list(self.context.telescopes, uuid=telescope_folder.uuid) is None:
+                    telescope_folder.read_remote_telescope()
         except ArcsecondConnectionError as error:
             self.context.messages['warning'] = str(error)
         else:
             if len(self.context.telescopes) == 0 and self.context.messages['warning'] == '':
                 msg = f'No telescopes detected. Make sure this folder or sub-ones contain a file named {OORT_FILENAME} '
-                msg += 'with a telescope UUID and relaunch command.'
+                msg += 'with a telescope UUID declared in a [telescope] section and relaunch command.'
                 self.context.messages['warning'] = msg
+
+    def walk_telescope_folders(self):
+        for telescope_folder in self.telescope_folders:
+            telescope_folder.walk()
 
     def upload_telescopes_calibrations(self):
         for telescope_folder in self.telescope_folders:
