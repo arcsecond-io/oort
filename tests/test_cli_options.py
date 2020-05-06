67646b5cb36e438b9687d3ab53c23813a69dfab3
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-05-06 19:08:16 +0200

    Fixing tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/test_cli_options.py
+++ tests/test_cli_options.py
@@ -1,26 +1,23 @@
 import re
 from click.testing import CliRunner
-from oort import server
+from oort import cli
 
 
 def test_cli_version():
     runner = CliRunner()
-    result = runner.invoke(server.main, ['-v'])
+    result = runner.invoke(cli.main, ['--version'])
     assert result.exit_code == 0 and not result.exception
     assert re.match('[0-9].[0-9].[0-9]', result.output)
-    result = runner.invoke(server.main, ['--version'])
-    assert result.exit_code == 0 and not result.exception
-    assert re.match('[0-9].[0-9].[0-9]', result.output)
-    result = runner.invoke(server.version)
+    result = runner.invoke(cli.version)
     assert result.exit_code == 0 and not result.exception
     assert re.match('[0-9].[0-9].[0-9]', result.output)
 
 
 def test_cli_global_help():
     runner = CliRunner()
-    result = runner.invoke(server.main, ['-h'])
+    result = runner.invoke(cli.main, ['-h'])
     assert result.exit_code == 0 and not result.exception
     assert 'Usage: main [OPTIONS] COMMAND [ARGS]' in result.output
-    result = runner.invoke(server.main, ['--help'])
+    result = runner.invoke(cli.main, ['--help'])
     assert result.exit_code == 0 and not result.exception
     assert 'Usage: main [OPTIONS] COMMAND [ARGS]' in result.output
