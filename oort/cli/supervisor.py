7b7069a838a345a4564a3990cb802964f5abcf68
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-13 17:31:41 +0200

    Setting up commands in supervisor conf

--- oort/cli/supervisor.py
+++ oort/cli/supervisor.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 import subprocess
 
@@ -5,7 +6,28 @@ from oort.config import get_supervisor_conf_file_path
 
 
 def setup_supervisor():
-    if not os.path.exists(get_supervisor_conf_file_path()):
+    conf_file_path = get_supervisor_conf_file_path()
+    
+    if not os.path.exists(conf_file_path):
         output = subprocess.run(["echo_supervisord_conf"], capture_output=True, text=True)
-        with open(get_supervisor_conf_file_path(), "w") as f:
-            f.write(output.stdout)
+        conf_content = output.stdout
+    else:
+        with open(conf_file_path, "r") as f:
+            conf_content = f.read()
+
+    spec = importlib.util.find_spec('oort')
+    server_command = os.path.join(os.path.dirname(spec.origin), 'server', 'main.py')
+    uploader_command = os.path.join(os.path.dirname(spec.origin), 'uploader', 'main.py')
+
+    if server_command not in conf_content:
+        conf_content += "\n\n"
+        conf_content += "[program:oort-server]\n"
+        conf_content += f"command={server_command}"
+
+    if uploader_command not in conf_content:
+        conf_content += "\n\n"
+        conf_content += "[program:oort-uploader]\n"
+        conf_content += f"command={uploader_command}"
+
+    with open(conf_file_path, "w") as f:
+        f.write(conf_content)
