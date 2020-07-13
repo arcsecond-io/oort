3dfa8cabe688d6a34da0bc41fbe8adf5ace7683e
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-13 17:35:10 +0200

    Permissions

--- oort/cli/supervisor.py
+++ oort/cli/supervisor.py
@@ -7,7 +7,7 @@ from oort.config import get_supervisor_conf_file_path
 
 def setup_supervisor():
     conf_file_path = get_supervisor_conf_file_path()
-    
+
     if not os.path.exists(conf_file_path):
         output = subprocess.run(["echo_supervisord_conf"], capture_output=True, text=True)
         conf_content = output.stdout
@@ -18,6 +18,10 @@ def setup_supervisor():
     spec = importlib.util.find_spec('oort')
     server_command = os.path.join(os.path.dirname(spec.origin), 'server', 'main.py')
     uploader_command = os.path.join(os.path.dirname(spec.origin), 'uploader', 'main.py')
+
+    # Making sure they are executable
+    os.chmod(server_command, 0o744)
+    os.chmod(uploader_command, 0o744)
 
     if server_command not in conf_content:
         conf_content += "\n\n"
