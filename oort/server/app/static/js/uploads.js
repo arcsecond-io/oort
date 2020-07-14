28739f320451a23e37b1c2ccd0128ec23b50b789
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-14 11:11:38 +0200

    Commenting out for now

--- oort/server/app/static/js/uploads.js
+++ oort/server/app/static/js/uploads.js
@@ -20,52 +20,52 @@ var app = new Vue({
     clearInterval(this.loopID)
   },
   mounted: function () {
-    const self = this
+    // const self = this
+    //
+    // this.source = new EventSource('/uploads')
+    // this.source.onmessage = function (event) {
+    //   self.isAlive = true
+    //   const json = JSON.parse(event.data)
+    //   self.state = json.state
+    //   self.messages = json.messages
+    //
+    //   self.telescopes = json.telescopes
+    //   self.night_logs = json.night_logs || []
+    //
+    //   if (self.night_logs.length > 0) {
+    //     self.selected_night_log_url = 'data/' + self.night_logs[0]['date'].replace(/-/gi, '/')
+    //   }
+    //
+    //   self.uploads = json.uploads
+    //   self.filtered_pending_uploads = self.uploads.filter(u => u.state === 'pending')
+    //   self.filtered_current_uploads = self.uploads.filter(u => u.state === 'current')
+    //   self.filtered_finished_uploads = self.uploads.filter(u => u.state === 'finished')
+    //
+    //   if (self.selected_telescope) {
+    //     self.filtered_pending_uploads = self.filtered_pending_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
+    //     self.filtered_current_uploads = self.filtered_current_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
+    //     self.filtered_finished_uploads = self.filtered_finished_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
+    //   }
+    //
+    //   self.filtered_pending_uploads((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
+    //   self.filtered_current_uploads.sort((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
+    //   self.filtered_finished_uploads.sort((u1, u2) => new Date(u1.ended).getDate() < new Date(u2.ended).getDate())
+    //
+    //   const bars = document.getElementsByClassName('progress-bar')
+    //   self.filtered_current_uploads.forEach((upload, index) => {
+    //     let bar = bars[index]
+    //     if (bar) {
+    //       bar.style.width = upload.progress.toFixed(1).toString() + '%'
+    //     }
+    //   })
+    // }
 
-    this.source = new EventSource('/uploads')
-    this.source.onmessage = function (event) {
-      self.isAlive = true
-      const json = JSON.parse(event.data)
-      self.state = json.state
-      self.messages = json.messages
-
-      self.telescopes = json.telescopes
-      self.night_logs = json.night_logs || []
-
-      if (self.night_logs.length > 0) {
-        self.selected_night_log_url = 'data/' + self.night_logs[0]['date'].replace(/-/gi, '/')
-      }
-
-      self.uploads = json.uploads
-      self.filtered_pending_uploads = self.uploads.filter(u => u.state === 'pending')
-      self.filtered_current_uploads = self.uploads.filter(u => u.state === 'current')
-      self.filtered_finished_uploads = self.uploads.filter(u => u.state === 'finished')
-
-      if (self.selected_telescope) {
-        self.filtered_pending_uploads = self.filtered_pending_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
-        self.filtered_current_uploads = self.filtered_current_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
-        self.filtered_finished_uploads = self.filtered_finished_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
-      }
-
-      self.filtered_pending_uploads((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
-      self.filtered_current_uploads.sort((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
-      self.filtered_finished_uploads.sort((u1, u2) => new Date(u1.ended).getDate() < new Date(u2.ended).getDate())
-
-      const bars = document.getElementsByClassName('progress-bar')
-      self.filtered_current_uploads.forEach((upload, index) => {
-        let bar = bars[index]
-        if (bar) {
-          bar.style.width = upload.progress.toFixed(1).toString() + '%'
-        }
-      })
-    }
-
-    this.loopID = setInterval(function ping () {
-      self.isAlive = (self.source.readyState <= 1)
-      if (!self.isAlive) {
-        self.$forceUpdate()
-      }
-    }, 3000)
+    // this.loopID = setInterval(function ping () {
+    //   self.isAlive = (self.source.readyState <= 1)
+    //   if (!self.isAlive) {
+    //     self.$forceUpdate()
+    //   }
+    // }, 3000)
   },
   methods: {
     selectTelescope (uuid) {
