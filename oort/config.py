ee2d197f1fe491200d30f16d20575f3f35f9265f
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-19 17:45:59 +0200

    Better config of logger
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/config.py
+++ oort/config.py
@@ -25,12 +25,16 @@ def get_supervisor_conf_file_path():
     return os.path.join(get_directory_path(), 'supervisord.conf')
 
 
-def get_logger():
+def get_logger(debug=False):
     logger = logging.getLogger('oort-cloud')
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
     fh = logging.FileHandler(get_log_file_path())
-    fh.setLevel(logging.INFO)
+    fh.setLevel(logging.DEBUG if debug else logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     logger.addHandler(fh)
+    if debug is True:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        logger.addHandler(console_handler)
     return logger
