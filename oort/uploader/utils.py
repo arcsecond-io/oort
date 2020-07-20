33f640f3b5623e756a17b939f3963eb8cef536d7
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-20 21:43:49 +0200

    Moving func to shared utils
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/uploader/utils.py
+++ oort/uploader/utils.py
@@ -4,31 +4,6 @@ import dateparser
 from astropy.io import fits as pyfits
 
 
-def find_first_in_list(objects, **kwargs):
-    return next((obj for obj in objects if
-                 len(set(obj.keys()).intersection(kwargs.keys())) > 0 and
-                 all([obj[k] == v for k, v in kwargs.items() if k in obj.keys()])),
-                None)
-
-
-class SafeDict(dict):
-    def __init__(self, *args):
-        dict.__init__(self, args)
-
-    def __getitem__(self, key):
-        try:
-            return super().__getitem__(key)
-        except KeyError:
-            return None
-
-    def append(self, key, *items):
-        if key not in self.keys():
-            self[key] = []
-        for item in items:
-            if item not in self[key]:
-                self[key].append(item)
-
-
 def find_fits_filedate(path, debug):
     file_date = None
     try:
