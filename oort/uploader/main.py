24c99974d82d8e76a5c2be7763add54c92a8f690
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-22 22:10:14 +0200

    Using path and leaving UploadPack for something else

--- oort/uploader/main.py
+++ oort/uploader/main.py
@@ -6,17 +6,16 @@ from watchdog.observers import Observer
 
 from oort.shared.config import get_logger
 
-dir_path = '.'
 
 class DataFileHandler(FileSystemEventHandler):
-    def __init__(self, pack: UploadPack):
+    def __init__(self, path: str):
         super().__init__()
+        self._path = path
         self._logger = get_logger()
-        self._pack = pack
 
     def run_initial_walk(self):
-        for file in os.listdir(self._pack.path):
-            filename = os.path.join(self._pack.path, file)
+        for file in os.listdir(self._path):
+            filename = os.path.join(self._path, file)
             event = FileCreatedEvent(filename)
             self.on_created(event)
 
@@ -33,19 +32,18 @@ class DataFileHandler(FileSystemEventHandler):
         self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
 
 
-
 class PathsObserver(Observer):
     def __init__(self):
         super().__init__()
         self._handler_mapping = {}
         self._watch_mapping = {}
 
-    def add(self, pack: UploadPack):
-        event_handler = DataFileHandler(pack=pack)
-        self._handler_mapping[pack.path] = event_handler
+    def add(self, path: str):
+        event_handler = DataFileHandler(path=path)
+        self._handler_mapping[path] = event_handler
         event_handler.run_initial_walk()
-        watch = self.schedule(event_handler, pack.path, recursive=True)
-        self._watch_mapping[pack.path] = watch
+        watch = self.schedule(event_handler, path, recursive=True)
+        self._watch_mapping[path] = watch
 
     def remove(self, path):
         if path in self._watch_mapping.keys():
