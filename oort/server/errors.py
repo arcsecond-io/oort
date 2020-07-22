c8482028820ebaef6a9c0fb1df55710c40f5bda9
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-22 22:09:42 +0200

    Adding necessary error

--- oort/server/errors.py
+++ oort/server/errors.py
@@ -11,6 +11,12 @@ class NotLoggedInOortCloudError(OortCloudError):
         super().__init__('You must login first: `arcsecond login`')
 
 
+class UnknownTelescopeOortCloudError(OortCloudError):
+    def __init__(self, telescope_uuid):
+        msg = f'Invalid / unknown telescopw with UUID {telescope_uuid}'
+        super().__init__(msg)
+
+
 class InvalidOrgMembershipOortCloudError(OortCloudError):
     def __init__(self, organisation):
         msg = f'Invalid / unknown membership for {organisation}. Login again: `arcsecond login --organisation {organisation}`'
