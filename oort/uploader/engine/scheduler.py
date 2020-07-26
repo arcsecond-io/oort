3e54a946a721c848bc8ad2a71a3f2060d4bf2f19
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-26 22:38:02 +0200

    New scheduler alogorithms
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/uploader/engine/scheduler.py
+++ oort/uploader/engine/scheduler.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from oort.shared.config import get_logger
 from .preparator import UploadPreparator
 from .uploader import FileUploader
@@ -9,20 +11,22 @@ MAX_TASKS = 3
 
 class UploadScheduler(object):
     def __init__(self):
-        self._queue = []
-        self._count = 0
+        # Creating a single async Queue
+        self._queue = asyncio.Queue()
+        # Creating a limited number of consumers to avoid too many simultaneous call to api.arcsecond.io
+        self._consumers = [asyncio.create_task(self._consumer(self._queue)) for _ in range(MAX_TASKS)]
 
-    def prepare_and_upload(self, preparator: UploadPreparator):
-        if self._count < MAX_TASKS:
-            self._count += 1
+    async def _consumer(self, queue):
+        while True:
+            preparator: UploadPreparator = await queue.get()
             await preparator.prepare()
             file_uploader = FileUploader(preparator.pack, preparator.identity)
             await file_uploader.upload()
-            self._count -= 1
-            if len(self._queue) > 0:
-                self.prepare_and_upload(self._queue.pop())
-        else:
-            self._queue.append(preparator)
+            queue.task_done()
+
+    def prepare_and_upload(self, preparator: UploadPreparator):
+        # Enquing new preparator.
+        self._queue.put(preparator)
 
 
 scheduler = UploadScheduler()
