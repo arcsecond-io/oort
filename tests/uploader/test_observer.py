c600e25fe7adb4fc8d785d7aecf57c9128197338
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-26 16:43:11 +0200

    Better tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/uploader/test_observer.py
+++ tests/uploader/test_observer.py
@@ -1,5 +1,6 @@
 from unittest.mock import ANY, patch
 
+from oort.shared.identity import Identity
 from oort.uploader.engine.eventhandler import DataFileHandler
 from oort.uploader.engine.pathsobserver import PathsObserver
 
@@ -13,6 +14,6 @@ def test_observer_adding_folder_path():
     po = PathsObserver()
     with patch.object(DataFileHandler, 'run_initial_walk'), \
          patch.object(PathsObserver, 'schedule') as mocked_method:
-        po.start_observe_folder('.', None)
+        po.start_observe_folder('.', Identity('cedric', debug=True))
         assert mocked_method.called_once_with(ANY, '.', recursive=True)
         assert po.observed_paths == ['.']
