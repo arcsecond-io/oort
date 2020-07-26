1fdef34d389dd474873850afaaf8f55db2d03ae8
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-26 22:37:42 +0200

    New errors
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/uploader/engine/errors.py
+++ oort/uploader/engine/errors.py
@@ -3,3 +3,19 @@ from oort.shared.errors import OortCloudError
 
 class UploadPreparationError(OortCloudError):
     pass
+
+
+class UploadPreparationAPIError(OortCloudError):
+    pass
+
+
+class UploadPreparationFatalError(OortCloudError):
+    pass
+
+
+class UploadPackingError(OortCloudError):
+    pass
+
+
+class PathObservationError(OortCloudError):
+    pass
