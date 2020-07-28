df79c78a5114ee4a7ccdd239dbdfc89a6f2232c1
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-07-28 09:16:06 +0200

    Updated scheduler

--- oort/uploader/engine/scheduler.py
+++ oort/uploader/engine/scheduler.py
@@ -11,17 +11,24 @@ MAX_TASKS = 3
 
 class UploadScheduler(object):
     def __init__(self):
+        self._loop = asyncio.get_event_loop()
+        # self._loop.run_forever()
         # Creating a single async Queue
-        self._queue = asyncio.Queue()
+        self._queue = asyncio.Queue(0, loop=self._loop)
         # Creating a limited number of consumers to avoid too many simultaneous call to api.arcsecond.io
-        self._consumers = [asyncio.create_task(self._consumer(self._queue)) for _ in range(MAX_TASKS)]
+        self._consumers = [self._loop.create_task(self._consumer(self._queue)) for _ in range(MAX_TASKS)]
+
+    def __del__(self):
+        for _consumer in self._consumers:
+            _consumer.cancel()
+        self._loop.close()
 
     async def _consumer(self, queue):
         while True:
             preparator: UploadPreparator = await queue.get()
             await preparator.prepare()
-            file_uploader = FileUploader(preparator.pack, preparator.identity)
-            await file_uploader.upload()
+            # file_uploader = FileUploader(preparator.pack, preparator.identity)
+            # await file_uploader.upload()
             queue.task_done()
 
     def prepare_and_upload(self, preparator: UploadPreparator):
