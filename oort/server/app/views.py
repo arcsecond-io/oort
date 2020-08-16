b050c5827df1a3b16e1df83600e7ac7235c0d8da
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-08-16 22:19:35 +0200

    Using new ArcsecondAPI
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/server/app/views.py
+++ oort/server/app/views.py
@@ -2,8 +2,8 @@ import json
 import time
 
 from arcsecond import ArcsecondAPI
+from flask import Blueprint, Response, render_template, request
 from flask import current_app as app, redirect, url_for
-from flask import render_template, Response, Blueprint, request
 
 from oort.shared.config import get_logger
 from .context import Context
@@ -21,10 +21,10 @@ def index():
 
 @main.route('/login', methods=['POST'])
 def login():
-    result, error = Arcsecond.login(request.form.get('username'),
-                                    request.form.get('password'),
-                                    request.form.get('subdomain'),
-                                    debug=app.config['context'].debug)
+    result, error = ArcsecondAPI.login(request.form.get('username'),
+                                       request.form.get('password'),
+                                       request.form.get('subdomain'),
+                                       debug=app.config['context'].debug)
     app.config['login_error'] = json.loads(error) if error else None
     return redirect(url_for('main.index'))
 
