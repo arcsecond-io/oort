18f213bf4d7ae83c39766588b7f7bab859a30829
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-08-18 22:14:52 +0200

    Simpler + better log message

--- oort/uploader/engine/eventhandler.py
+++ oort/uploader/engine/eventhandler.py
@@ -34,16 +34,16 @@ class DataFileHandler(FileSystemEventHandler):
             self.on_created(event)
 
     def on_created(self, event):
-        self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
+        self._logger.info(f'Created event for path : {event.src_path}')
         pack = UploadPack(self._path, event.src_path)
         preparator = UploadPreparator(pack=pack, identity=self._identity, debug=self._debug)
         scheduler.prepare_and_upload(preparator)
 
-    def on_moved(self, event):
-        self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
-
-    def on_deleted(self, event):
-        self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
-
-    def on_modified(self, event):
-        self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
+    # def on_moved(self, event):
+    #     self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
+    #
+    # def on_deleted(self, event):
+    #     self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
+    #
+    # def on_modified(self, event):
+    #     self._logger.info(f'event type: {event.event_type}  path : {event.src_path}')
