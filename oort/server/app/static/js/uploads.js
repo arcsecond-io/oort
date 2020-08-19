78821de1d545c2306bbedff7da2864f0f39c1f3e
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-08-19 21:46:54 +0200

    Improved html tables

--- oort/server/app/static/js/uploads.js
+++ oort/server/app/static/js/uploads.js
@@ -7,16 +7,13 @@ var app = new Vue({
     loopID: null,
     state: { folders: [] },
     selected_folder: null,
+    pending_uploads: [],
+    current_uploads: [],
+    finished_uploads: [],
+    error_uploads: [],
     messages: {},
     telescopes: [],
-    night_logs: [],
-    selected_telescope: null,
-    selected_night_log: null,
-    selected_night_log_url: '',
-    uploads: [],
-    filtered_pending_uploads: [],
-    filtered_current_uploads: [],
-    filtered_finished_uploads: []
+    night_logs: []
   },
   beforeDestroy () {
     clearInterval(this.loopID)
@@ -30,36 +27,18 @@ var app = new Vue({
     const self = this
     this.source = new EventSource('/uploads')
     this.source.onmessage = function (event) {
-      const json = JSON.parse(event.data)
-      self.state = json.state
+      const json_data = JSON.parse(event.data)
+
+      self.state = json_data.state
       if (self.state.folders.length === 1) {
         self.selected_folder = self.state.folders[0]
       }
 
-      //   self.messages = json.messages
-      //
-      //   self.telescopes = json.telescopes
-      //   self.night_logs = json.night_logs || []
-      //
-      //   if (self.night_logs.length > 0) {
-      //     self.selected_night_log_url = 'data/' + self.night_logs[0]['date'].replace(/-/gi, '/')
-      //   }
-      //
-      //   self.uploads = json.uploads
-      self.pending_uploads = json.pending
-      self.current_uploads = json.current
-      self.finished_uploads = json.finished
+      self.pending_uploads = json_data.pending
+      self.current_uploads = json_data.current
+      self.finished_uploads = json_data.finished
+      self.error_uploads = json_data.error
 
-      //   if (self.selected_telescope) {
-      //     self.filtered_pending_uploads = self.filtered_pending_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
-      //     self.filtered_current_uploads = self.filtered_current_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
-      //     self.filtered_finished_uploads = self.filtered_finished_uploads.filter(u => u.telescope['uuid'] === self.selected_telescope['uuid'])
-      //   }
-      //
-      //   self.filtered_pending_uploads((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
-      //   self.filtered_current_uploads.sort((u1, u2) => new Date(u1.started).getDate() < new Date(u2.started).getDate())
-      //   self.filtered_finished_uploads.sort((u1, u2) => new Date(u1.ended).getDate() < new Date(u2.ended).getDate())
-      //
       const bars = document.getElementsByClassName('progress-bar')
       self.current_uploads.forEach((upload, index) => {
         let bar = bars[index]
@@ -70,32 +49,6 @@ var app = new Vue({
     }
   },
   methods: {
-    selectTelescope (uuid) {
-      this.selected_telescope = (uuid === '__all__') ? null : this.telescopes.find(t => t.uuid === uuid)
-      let pending_uploads = this.uploads.filter(u => u.state === 'pending')
-      let current_uploads = this.uploads.filter(u => u.state === 'current')
-      let finished_uploads = this.uploads.filter(u => u.state === 'finished')
-
-      if (this.selected_telescope) {
-        this.selected_night_log = this.night_logs.find(nl => nl.telescope === uuid)
-        this.filtered_pending_uploads = pending_uploads.filter(u => u.telescope['uuid'] === this.selected_telescope['uuid'])
-        this.filtered_current_uploads = current_uploads.filter(u => u.telescope['uuid'] === this.selected_telescope['uuid'])
-        this.filtered_finished_uploads = finished_uploads.filter(u => u.telescope['uuid'] === this.selected_telescope['uuid'])
-      } else {
-        this.selected_night_log = null
-        this.filtered_pending_uploads = pending_uploads
-        this.filtered_current_uploads = current_uploads
-        this.filtered_finished_uploads = finished_uploads
-      }
-      if (this.selected_night_log) {
-        this.selected_night_log_url = 'data/' + this.selected_night_log['date'].replace(/-/gi, '/')
-      } else if (this.night_logs.length > 0) {
-        this.selected_night_log_url = 'data/' + this.night_logs[0]['date'].replace(/-/gi, '/')
-      } else {
-        this.selected_night_log_url = ''
-      }
-      this.$forceUpdate()
-    },
     getFormattedSize (bytes, decimals) {
       if (bytes === 0) return '0 Bytes'
       const k = 1024
