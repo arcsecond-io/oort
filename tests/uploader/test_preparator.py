c00f0907e545e329d200c63234e9941f7726f5a6
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-08-30 13:48:42 +0200

    Making all tests pass
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/uploader/test_preparator.py
+++ tests/uploader/test_preparator.py
@@ -14,14 +14,15 @@ from tests.utils import use_test_database
 spec = importlib.util.find_spec('oort')
 
 folder_path = os.path.join(os.path.dirname(spec.origin), '..', 'tests', 'fixtures')
-fits_file_path = os.path.join(folder_path, 'very_simple.fits')
+fits_file_name = 'very_simple.fits'
+fits_file_path = os.path.join(folder_path, fits_file_name)
 
 telescope_uuid = '44f5bee9-a557-4264-86d6-c877d5013788'
-identity = Identity('cedric', str(uuid.uuid4()), 'saao', 'admin', telescope_uuid)
 
 
 @use_test_database
 def test_preparator_init_no_org():
+    identity = Identity('cedric', str(uuid.uuid4()), debug=True)
     pack = UploadPack(folder_path, fits_file_path, identity)
     with patch.object(UploadPreparator, 'prepare') as mock_method:
         prep = UploadPreparator(pack, Identity('cedric', str(uuid.uuid4()), debug=True))
@@ -37,16 +38,21 @@ def test_preparator_init_no_org():
 
 @use_test_database
 def test_preparator_init_with_org():
+    identity = Identity('cedric', str(uuid.uuid4()), 'saao', 'admin', telescope_uuid)
     pack = UploadPack(folder_path, fits_file_path, identity)
-    with patch.object(UploadPreparator, 'prepare') as mock_method:
+    with patch.object(UploadPreparator, 'prepare') as mock_method, \
+            patch.object(ArcsecondAPI, 'read', return_value=({'subdomain': 'saao'}, None)) as mock_method_read:
         assert Organisation.select().count() == 0
         prep = UploadPreparator(pack, identity)
+
         assert prep is not None
         assert prep.preparation_succeeded is False
         assert prep.telescope is None
         assert prep.night_log is None
         assert prep.obs_or_calib is None
         assert prep.dataset is None
+
+        assert mock_method_read.called_once_with(date=pack.night_log_date_string)
         org = Organisation.select(Organisation.subdomain == 'saao').get()
         assert org is not None
 
@@ -59,8 +65,8 @@ def test_preparator_prepare_no_org_no_telescope():
     assert identity.telescope is None
 
     nl = {'uuid': str(uuid.uuid4()), 'date': pack.night_log_date_string}
-    obs = {'uuid': str(uuid.uuid4()), 'night_log': nl['uuid'], 'name': 'observation!'}
-    ds = {'uuid': str(uuid.uuid4()), 'observation': obs['uuid'], 'name': 'dataset!'}
+    obs = {'uuid': str(uuid.uuid4()), 'night_log': nl['uuid'], 'name': pack.dataset_name}
+    ds = {'uuid': str(uuid.uuid4()), 'observation': obs['uuid'], 'name': pack.dataset_name}
 
     with patch.object(ArcsecondAPI, 'list', return_value=([], None)) as mock_method_list, \
             patch.object(ArcsecondAPI, 'create') as mock_method_create:
@@ -70,8 +76,13 @@ def test_preparator_prepare_no_org_no_telescope():
         up.prepare()
         assert mock_method_list.called_once_with(date=pack.night_log_date_string)
         assert mock_method_create.called_with(**nl)
+        assert up.night_log is not None
         assert up.night_log.uuid == nl['uuid']
+
         assert mock_method_create.called_with(**obs)
+        assert up.obs_or_calib is not None
         assert up.obs_or_calib.uuid == obs['uuid']
+
         assert mock_method_create.called_with(**ds)
+        assert up.dataset is not None
         assert up.dataset.uuid == ds['uuid']
