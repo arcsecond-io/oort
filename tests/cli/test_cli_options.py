b533aec705251bc49d0cb9e765452c37a0e2e192
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-08-30 18:42:06 +0200

    Fixing tests
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/cli/test_cli_options.py
+++ tests/cli/test_cli_options.py
@@ -1,17 +1,19 @@
 import re
+
 from click.testing import CliRunner
-from oort import cli
+
+from oort.cli.cli import main
 
 
 def test_cli_version():
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['--version'])
+    result = runner.invoke(main, ['--version'])
     assert result.exit_code == 0 and not result.exception
     assert re.match('[0-9].[0-9].[0-9]', result.output)
 
 
 def test_cli_global_help():
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['--help'])
+    result = runner.invoke(main, ['--help'])
     assert result.exit_code == 0 and not result.exception
     assert 'Usage: main [OPTIONS] COMMAND [ARGS]' in result.output
