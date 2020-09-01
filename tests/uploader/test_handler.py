2abd04bc4c26e023bb9c06bb4f42f10a7100b143
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-09-01 21:18:30 +0200

    Adding test
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/uploader/test_handler.py
+++ tests/uploader/test_handler.py
@@ -1,12 +1,11 @@
 import importlib
 import os
+import time
 import uuid
 from unittest.mock import patch
 
 from oort.shared.identity import Identity
-from oort.shared.models import Substatus, upload_post_save_signal, Upload
 from oort.uploader.engine.eventhandler import DataFileHandler
-from oort.uploader.engine.packer import UploadPack
 from tests.utils import use_test_database
 
 spec = importlib.util.find_spec('oort')
@@ -17,19 +16,15 @@ fits_file_path = os.path.join(folder_path, fits_file_name)
 
 telescope_uuid = '44f5bee9-a557-4264-86d6-c877d5013788'
 
-#
-# @use_test_database
-# def test_event_handler_react_to_upload_save():
-#     identity = Identity('cedric', str(uuid.uuid4()), 'saao', 'admin', telescope_uuid)
-#
-#     with patch.object(DataFileHandler, 'on_save_handler') as mock_method:
-#         # To avoid a mock error:
-#         mock_method.__name__ = 'mock on_save_handler method'
-#
-#         df = DataFileHandler(folder_path, identity, debug=True)
-#         upload_post_save_signal.connect(df.on_save_handler, sender=Upload)
-#         pack = UploadPack(folder_path, fits_file_path, identity)
-#         assert mock_method.call_count == 1
-#
-#         pack.upload.smart_update(substatus=Substatus.CHECKING.value)
-#         assert mock_method.call_count == 2
+
+@use_test_database
+def test_event_handler_react_to_upload_save():
+    identity = Identity('cedric', str(uuid.uuid4()), 'saao', 'admin', telescope_uuid)
+
+    with patch.object(DataFileHandler, '_restart_uploads') as mock_method:
+        # To avoid a mock error:
+        mock_method.__name__ = 'mock on_save_handler method'
+
+        df = DataFileHandler(folder_path, identity, debug=True)
+        time.sleep(5.5)
+        assert mock_method.call_count == 1
