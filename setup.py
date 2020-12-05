c85e518dbcd60e0b0075106fabe4baa322937395
Cédric Foellmi <cedric@onekiloparsec.dev>
2020-12-05 12:18:29 +0100

    Making sure of some versions
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- setup.py
+++ setup.py
@@ -33,12 +33,12 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'astropy',
-        'flask',
-        'peewee',
-        'watchdog',
-        'supervisor',
         'arcsecond>=1.2.2',
+        'astropy>=4',
+        'flask>=1.1',
+        'peewee>=3',
+        'watchdog>=0.10',
+        'supervisor>=4.2',
         'dateparser',
         'python-dotenv'
     ],
