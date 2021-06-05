5a18093a2a27e0d0b8b89d22e19db61f533e7ceb
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-06-05 10:00:09 +0200

    Using new field upload_key
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/cli/cli.py
+++ oort/cli/cli.py
@@ -261,7 +261,7 @@ def folders(state):
         for index, section in enumerate(sections):
             click.echo(f" • Folder #{index + 1}:")
             click.echo(f"   username     = @{section.get('username')}")
-            click.echo(f"   api_key      = {section.get('api_key')[0:4]}•••••••")
+            click.echo(f"   upload_key   = {section.get('upload_key')[0:4]}•••••••")
             if section.get('subdomain'):
                 click.echo(f"   organisation = {section.get('subdomain')} (role: {section.get('role')})")
             else:
