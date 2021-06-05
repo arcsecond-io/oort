1a09bdf5d527b920f7c95ac52c42d9ded93741d2
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-06-05 12:37:23 +0200

    Requiring new Arcsecond version
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'arcsecond>=1.3.1',
+        'arcsecond>=1.3.2',
         'astropy>=4',
         'flask>=1.1',
         'peewee>=3',
