569979fad8f78b284cf5c36cd387f17c2e91307b
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-08-14 14:21:07 +0200

    Let the uploader process to the actual detection of folders!
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/uploader/main.py
+++ oort/uploader/main.py
@@ -3,14 +3,13 @@ import signal
 import sys
 import time
 
-from oort.shared.config import get_oort_config_upload_folder_sections, get_oort_logger
-from oort.shared.identity import Identity
+from oort.shared.config import get_oort_logger
 from oort.uploader.engine.pathsobserver import PathsObserver
 from oort.uploader.engine.zipper import zipper_stop_event
 
-paths_observer = PathsObserver()
-
 logger = get_oort_logger('uploader')
+
+paths_observer = PathsObserver()
 
 
 def handle_ctrl_c(signum, frame):
@@ -30,14 +29,8 @@ if __name__ == "__main__":
     debug = len(sys.argv) > 1 and sys.argv[1] in ['-d', '--debug']
     paths_observer.debug = debug
 
-    logger.info('Starting infinite loop of PathsObserver...')
+    logger.info('Starting thread of PathsObserver...')
     paths_observer.start()
-
-    for folder_section in get_oort_config_upload_folder_sections():
-        identity = Identity.from_folder_section(folder_section)
-        folder_path = folder_section.get('path')
-        # paths_observer.observe_folder will deal with the initial walk.
-        paths_observer.observe_folder(folder_path, identity)
 
     try:
         while paths_observer.is_alive():
