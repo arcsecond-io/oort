6b17116c2c9cd28960b6c523089ef74e58123e4f
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-08-14 14:47:12 +0200

    Detection of watched folder occuring inside pathsobserver
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/uploader/engine/pathsobserver.py
+++ oort/uploader/engine/pathsobserver.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from pathlib import Path
 from typing import List
@@ -5,7 +6,7 @@ from typing import List
 from watchdog.events import FileCreatedEvent
 from watchdog.observers import Observer
 
-from oort.shared.config import get_oort_logger
+from oort.shared.config import get_oort_config_upload_folder_sections, get_oort_logger
 from oort.shared.constants import OORT_UPLOADER_FOLDER_DETECTION_TICK_SECONDS
 from oort.shared.identity import Identity
 from oort.shared.models import Upload
@@ -35,7 +36,29 @@ class PathsObserver(Observer):
     def log_prefix(self) -> str:
         return '[PathsObserver]'
 
-    def _perform_initial_walk(self, folder_path, event_handler):
+    def _detect_watched_folders(self):
+        for folder_section in get_oort_config_upload_folder_sections():
+            folder_path = folder_section.get('path')
+            if folder_path not in self._mapping.keys():
+                identity = Identity.from_folder_section(folder_section)
+                self._start_watching_folder(folder_path, identity)
+        threading.Timer(30, self._detect_watched_folders).start()
+
+    def _start_watching_folder(self, folder_path: str, identity: Identity, tick=5.0) -> None:
+        if folder_path in self._mapping.keys():
+            self._logger.warn(f'{self.log_prefix} Folder path {folder_path} already observed. Ignoring.')
+            return
+
+        self._logger.info(f'{self.log_prefix} Starting to watch folder {folder_path}...')
+        event_handler = eventhandler.DataFileHandler(path=folder_path, identity=identity, tick=tick, debug=self._debug)
+        watcher = self.schedule(event_handler, folder_path, recursive=True)
+        self._mapping[folder_path] = {'watcher': watcher, 'handler': event_handler}
+
+        threading.Thread(target=self._start_initial_walk, args=(folder_path, event_handler)).start()
+
+    def _start_initial_walk(self, folder_path, event_handler):
+        self._logger.info(f'{self.log_prefix} Starting initial walk inside folder {folder_path}...')
+
         root_path = Path(folder_path)
         # Just in case we pass a file...
         if root_path.is_file():
@@ -66,13 +89,6 @@ class PathsObserver(Observer):
         msg += f'dispatched {event_count} events for {file_count} files.'
         self._logger.info(msg)
 
-    def observe_folder(self, folder_path: str, identity: Identity, tick=5.0) -> None:
-        event_handler = eventhandler.DataFileHandler(path=folder_path, identity=identity, tick=tick, debug=self._debug)
-        self._logger.info(f'{self.log_prefix} Starting initial walk inside folder {folder_path}')
-        self._perform_initial_walk(folder_path, event_handler)
-        self._logger.info(f'{self.log_prefix} Starting to observe folder {folder_path}')
-        watch = self.schedule(event_handler, folder_path, recursive=True)
-        self._mapping[folder_path] = {'watcher': watch, 'handler': event_handler}
         event_handler.launch_restart_loop()
 
     def forget_folder(self, folder_path: str) -> None:
