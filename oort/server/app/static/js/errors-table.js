7cef9c74e95d5f80e868cd22b44906a98e733fd0
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-08-15 10:32:11 +0200

    Adding dataset in error table
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/server/app/static/js/errors-table.js
+++ oort/server/app/static/js/errors-table.js
@@ -4,6 +4,7 @@ Vue.component('errors-table', {
   <thead>
     <tr>
       <th>Filename</th>
+      <th>Dataset</th>
       <th>Owner</th>
       <th>Size</th>
       <th>Reason</th>
@@ -16,6 +17,17 @@ Vue.component('errors-table', {
       <span class="subtitle">&lt;root&gt;/</span><span><strong>{{ getFilePath(upload, root_path) }}</strong></span>
       <div><span class="subtitle">Obs Date:</span> {{ upload.file_date }}</div>
     </td>
+    <td>
+      <div v-if="upload.dataset">
+        <a v-if="upload.organisation" :href='getOrganisationDatasetURL(upload)' target="_blank">
+          {{ upload.dataset.name }}
+        </a>
+        <a v-else :href='getDatasetURL(upload)' target="_blank">
+          {{ upload.dataset.name }}
+        </a>
+        <div class="subtitle">{{ upload.dataset.uuid }}</div>
+      </div>
+    </td>    
     <td>
       <a v-if="upload.organisation" :href='getOrganisationURL(upload)' target="_blank">{{ upload.organisation.subdomain }}</a>
       <a v-else-if="upload.astronomer" :href='getProfileURL(upload)' target="_blank">@{{ upload.astronomer }}</a>
