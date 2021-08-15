5f96e4a02c46fb18fdb78ac50a7cc2716d701d3f
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-08-15 19:01:12 +0200

    Bumping Arcsecond version number
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'arcsecond>=1.3.7',
+        'arcsecond>=1.3.8',
         'astropy>=4',
         'flask>=1.1',
         'peewee>=3',
