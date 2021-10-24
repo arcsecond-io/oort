63e333721248b53e385f2e3046399c956e233d9e
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-10-24 15:08:04 +0200

    Making synchronous walker using two passes
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- oort/uploader/engine/walker.py
+++ oort/uploader/engine/walker.py
@@ -24,18 +24,39 @@ def walk(folder_string: str, identity: Identity, force, debug: bool):
     if root_path.is_file():
         root_path = root_path.parent
 
-    failed_uploads = []
-    success_uploads = []
+    total_file_count = sum(1 for f in root_path.glob('**/*') if f.is_file() and not is_hidden(f))
+
+    # --- first pass
+    logger.info(f"{log_prefix} Making a first pass to collect info on files...")
 
     index = 0
-    total_count = sum(1 for f in root_path.glob('**/*') if f.is_file() and not is_hidden(f))
     for file_path in root_path.glob('**/*'):
         # Skipping both hidden files and hidden directories.
         if is_hidden(file_path) or not file_path.is_file():
             continue
 
         index += 1
-        click.echo(f"\n{log_prefix} File {index} / {total_count} ({index / total_count * 100:.2f}%)\n")
+        click.echo(f"\n{log_prefix} File {index} / {total_file_count} ({index / total_file_count * 100:.2f}%)\n")
+
+        pack = packer.UploadPack(str(root_path), str(file_path), identity, force=force)
+        pack.collect_file_info()
+
+    logger.info(f"\n{log_prefix} Finished collecting file info inside folder {folder_string}.\n")
+
+    # --- second pass
+    logger.info(f"{log_prefix} Starting second pass to actually upload files...")
+
+    failed_uploads = []
+    success_uploads = []
+
+    index = 0
+    for file_path in root_path.glob('**/*'):
+        # Skipping both hidden files and hidden directories.
+        if is_hidden(file_path) or not file_path.is_file():
+            continue
+
+        index += 1
+        click.echo(f"\n{log_prefix} File {index} / {total_file_count} ({index / total_file_count * 100:.2f}%)\n")
 
         pack = packer.UploadPack(str(root_path), str(file_path), identity, force=force)
         status, substatus, error = pack.prepare_and_upload_file(display_progress=True)
@@ -44,14 +65,16 @@ def walk(folder_string: str, identity: Identity, force, debug: bool):
         else:
             failed_uploads.append((str(file_path), substatus, error))
 
-    msg = f'{log_prefix} Finished upload walk inside folder {folder_string} '
-    msg += f'with {len(success_uploads)} successful uploads and {len(failed_uploads)} failed.'
+    msg = f"\n{log_prefix} Finished upload walk inside folder {folder_string} "
+    msg += f"with {len(success_uploads)} successful uploads and {len(failed_uploads)} failed."
     logger.info(msg)
 
     if len(failed_uploads) > 0:
         logger.error(f'{log_prefix} Here are the failed uploads:')
         for path, substatus, error in failed_uploads:
             logger.error(f'{path} ({substatus}) {error}')
+
+    logger.info("\n")
 
 # if __name__ == '__main__':
 #     root_path = sys.argv[1]
