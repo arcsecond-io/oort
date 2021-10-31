badc567eb038365396e933c11e173b08f9c63b4c
Cédric Foellmi <cedric@onekiloparsec.dev>
2021-10-31 18:03:07 +0100

    Fixed imports
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- tests/uploader/test_observer.py
+++ tests/uploader/test_observer.py
@@ -2,17 +2,17 @@ import threading
 from unittest.mock import ANY, patch
 
 from oort.shared.identity import Identity
-from oort.uploader.engine.pathsobserver import PathsObserver
+from oort.uploader.engine.pathsobserver import PathObserverManager
 from tests.utils import use_test_database
 
 
 @use_test_database
 def test_observer_empty():
-    with patch.object(PathsObserver, 'schedule', return_value={}) as mocked_method_schedule, \
-            patch.object(PathsObserver, '_start_initial_walk', return_value={}) as mocked_method_walk, \
+    with patch.object(PathObserverManager, 'schedule', return_value={}) as mocked_method_schedule, \
+            patch.object(PathObserverManager, '_start_initial_walk', return_value={}) as mocked_method_walk, \
             patch.object(threading.Thread, 'start'), \
             patch.object(threading.Timer, 'start'):
-        po = PathsObserver()
+        po = PathObserverManager()
         assert po.observed_paths == []
         mocked_method_schedule.assert_not_called()
         mocked_method_walk.assert_not_called()
@@ -20,10 +20,10 @@ def test_observer_empty():
 
 @use_test_database
 def test_observer_adding_folder_path():
-    with patch.object(PathsObserver, 'schedule', return_value={}) as mocked_method_schedule, \
-            patch.object(PathsObserver, '_start_initial_walk', return_value={}) as mocked_method_walk, \
+    with patch.object(PathObserverManager, 'schedule', return_value={}) as mocked_method_schedule, \
+            patch.object(PathObserverManager, '_start_initial_walk', return_value={}) as mocked_method_walk, \
             patch.object(threading.Timer, 'start'):
-        po = PathsObserver()
+        po = PathObserverManager()
         po._schedule_watch('.', Identity('cedric', '123', debug=True), True)
         mocked_method_walk.assert_called()
         mocked_method_schedule.assert_called_once_with(ANY, '.', recursive=True)
